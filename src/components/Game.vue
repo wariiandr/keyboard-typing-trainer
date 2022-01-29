64c27927d821395a8672bbeeda1df0274849505b
wari1aandr <qalska7@gmail.com>
2022-01-29 23:55:40 +0300

    main, переход на game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,31 @@
 <template>
-    <div class="game d-flex flex-column 
-        justify-content-center align-items-center">
+    <div class="game mt-5">
+        <div class="game__words">
+            asd asd
+        </div>
 
+        <input type="text" name="text" id="text" class="game__input">
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            time: 60,
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.game {
+    width: 50%;
+}
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="main">
-        
+        <div v-if="!isStart" class="d-flex justify-content-center mt-5">
+            <img class="main__img shadow bg-body" src="/img/keyboard.jpg" alt="keyboard">
+
+            <div class="main__content ml-5">
+                <h1 class="main__h1">Keyboard Typing Trainer</h1>
+                <p class="mt-5">Find out your typing speed</p>
+                <button class="btn" type="submit" @click="isStart = true">Let's go!</button>
+            </div>
+        </div>
+
+        <game 
+            v-if="isStart"
+        />
     </div>
 </template>
 
 <script>
+import Game from '../components/Game.vue';
+
 export default {
     components: {
-
+        Game 
     },
     data() {
         return {
-
+            isStart: false,
         }
     },
     methods: {
@@ -24,5 +38,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.main {
+    &__h1 {
+        color: #0088cc;
+    }
+    &__img {
+        width: 35%;
+    }
+    &__content {
+        margin-left: 50px;
+    }
+}
+.btn {
+    background-color: #0088cc;
+    color: white;
+    &:hover {
+        background-color: #007ab8;
+        color: white;
+    }
+}
 </style>
